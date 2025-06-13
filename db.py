532db94675de1d775e594d95d1a0390d0591ca1b
Jpconwi <jpconwi2005@gmail.com>
2025-06-13 19:04:20 +0800

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-import os
-import urllib.parse as up
-
-def get_connection():
-    up.uses_netloc.append("postgres")
-    url = os.environ["DATABASE_URL"]
-    return psycopg2.connect(url)
