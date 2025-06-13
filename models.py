6611842689de1833d7d1bce2464cc0dfe8528d6e
Jpconwi <jpconwi2005@gmail.com>
2025-06-13 19:04:31 +0800

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-def format_messages(rows):
-    return [
-        {
-            "sender": row[0],
-            "content": row[1],
-            "timestamp": row[2].isoformat()
-        }
-        for row in rows
-    ]
