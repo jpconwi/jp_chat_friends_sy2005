dc34ac7bf0a76b0c6df0bee7d25192185b1c15a7
Jpconwi <jpconwi2005@gmail.com>
2025-06-13 19:32:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from db import get_connection
 
 app = Flask(__name__)
@@ -11,6 +11,26 @@ CORS(app)
 def home():
     return render_template("index.html")
 
+# --- Route:Register ---
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.json
+    conn = get_connection()
+    cur = conn.cursor()
+    try:
+        hashed_pw = generate_password_hash(data["password"])
+        cur.execute(
+            "INSERT INTO users (username, email, password_hash) VALUES (%s, %s, %s)",
+            (data["username"], data["email"], hashed_pw),
+        )
+        conn.commit()
+        return jsonify({"status": "registered"}), 201
+    except Exception as e:
+        print("Registration error:", e)
+        return jsonify({"status": "failed"}), 400
+    finally:
+        cur.close()
+        conn.close()
 # --- Route: Login ---
 @app.route("/login", methods=["POST"])
 def login():
